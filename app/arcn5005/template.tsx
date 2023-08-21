2e5e189df045436c804a7d39dbef71fe960e9541
Nico <nico14@gmail.com>
2023-08-21 15:39:30 -0400

    fixing deployments issues

--- app/arcn5005/template.tsx
+++ app/arcn5005/template.tsx
@@ -1,31 +1,11 @@
 "use client";
-import { useContext, useState } from "react";
-import { useRouter } from "next/navigation";
 
 import Header from "@/components/Header";
-import { mainSlides } from "@/lib/arcn5005/MainSlides";
 import { AppProvider } from "@/middleware/CombineProviders";
-import Stack from "@mui/material/Stack";
 
-import Pagination from "@mui/material/Pagination";
-import PaginationItem from "@mui/material/PaginationItem";
-import BackIcon from "@mui/icons-material/ArrowBackIosRounded";
-import ForwardIcon from "@mui/icons-material/ArrowForwardIosRounded";
-import { SlidesContext } from "@/middleware/Slides/context";
 import Footer from "@/components/Footer";
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  const { currentSlideNumber } = useContext(SlidesContext)["state"]["slides"];
-  const [page, setPage] = useState(currentSlideNumber + 1);
-
-  const router = useRouter();
-  function handlePaginationChange(e, value) {
-    console.log(e, value);
-    const index = value - 1;
-    setPage(index);
-    router.replace(mainSlides[index].url);
-  }
-
   return (
     <main className="flex flex-col w-screen h-screen bg-slate-100 justify-between">
       <AppProvider>
