7293cf128fa92ace72a71691734a870960aee72b
Nico <nico14@gmail.com>
2023-08-28 16:02:41 -0400

    removing overlapping template

--- app/arcn5005/f2023/students/[username]/[assignments]/page.tsx
+++ app/arcn5005/f2023/students/[username]/[assignments]/page.tsx
@@ -1,11 +1,4 @@
 "use client";
-import { classListf2023 } from "../../(classList)/classList";
-import { Header } from "../../../../../../components/Header";
-import { useParams } from "next/navigation";
-import { IconButton } from "@mui/material";
-import { useRouter } from "next/navigation";
-import ReturnButton from "../../../../../../components/Common/ReturnButton";
-import Avatar from "@mui/material/Avatar";
 import { FC } from "react";
 
 interface Props {
@@ -13,41 +6,11 @@ interface Props {
 }
 
 const page: FC<Props> = ({ params }) => {
-  const student = classListf2023.find(
-    (student) => student.username === params.username
-  );
   return (
-    <main className="flex flex-col w-screen h-screen justify-between">
-      <nav className="top-0 flex flex-row w-screen h-24 ">
-        <Header
-          title={`${
-            student
-              ? `${student.firstName} ${student.lastName}`
-              : "Unregistered Student"
-          } ${params.assignments ? `- ${params.assignments} assignment` : ""}`}
-        />
-      </nav>
-      {Boolean(student) ? (
-        <section className="flex justify-center grow">
-          <iframe
-            src={`/arcn5005/f2023/students/${params.username}/${params.assignments}/index.html`}
-            className="grow"
-          />
-        </section>
-      ) : (
-        <section className="flex justify-center grow items-center">
-          <ReturnButton />
-        </section>
-      )}
-      <footer className="m-4 flex flex-row-reverse absolute z-20 bottom-0 right-0">
-        <IconButton>
-          <Avatar
-            src={`/arcn5005/f2023/students/${params.username}/avatar.jpg`}
-            sx={{ width: 45, height: 45 }}
-          />
-        </IconButton>
-      </footer>
-    </main>
+    <iframe
+      src={`/arcn5005/f2023/students/${params.username}/${params.assignments}/index.html`}
+      className="grow z-0"
+    />
   );
 };
 
