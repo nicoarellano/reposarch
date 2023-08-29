a4296003dd50440b26fa070433c6dfd8d05c47cf
Nico <nico14@gmail.com>
2023-08-29 16:43:49 -0400

    wip resources

---
+++ app/arcn5005/(main)/resources/Books.ts
@@ -0,0 +1,16 @@
+import { Resources } from "../../../../types/types";
+
+export const Books: Resources = [
+  {
+    title: "Mario Carpo - The Alphabeth and the Algorithm",
+    url: "https://courseworks2.columbia.edu/files/594343/download?download_frd=1",
+  },
+  {
+    title: "Eric Raymond - The Cathedral and the Bazaar",
+    url: "https://monoskop.org/images/e/e0/Raymond_Eric_S_The_Cathedral_and_the_Bazaar_rev_ed.pdf",
+  },
+  {
+    title: "Douglas  - Program or be programmed",
+    url: "https://addc401fall2016.files.wordpress.com/2016/08/rushkoff-program-or-be-programmed.pdf",
+  },
+];
---
+++ app/arcn5005/(main)/resources/Websites.ts
@@ -0,0 +1,16 @@
+import { Resources } from "../../../../types/types";
+
+export const Websites: Resources = [
+  {
+    title: "OSArch",
+    url: "https://osarch.org/",
+  },
+  {
+    title: "IFC.js",
+    url: "https://ifcjs.github.io/info/",
+  },
+  {
+    title: "Blender",
+    url: "https://www.blender.org/",
+  },
+];
