5ce8cc11ff8a99dd5a572f3acc8519f8a17e8ed5
Nico <nico14@gmail.com>
2023-09-06 10:48:02 -0400

    wip c3

--- app/arcn5005/(lectures)/week03/index.tsx
+++ app/arcn5005/(lectures)/week03/index.tsx
@@ -2,18 +2,47 @@ import { Toc } from "../../../../types/types";
 import Image from "../../../../components/Common/Image";
 import Video from "../../../../components/Common/Video";
 
-import WhatIsBim from "./WhatIsBim.mdx";
-import BimOrCad from "./BimOrCad.mdx";
-import BimBooks from "./BimBooks.mdx";
-import Levels from "./Levels.mdx";
-import Mandates from "./Mandates.mdx";
-import RevitOrArchicad from "./RevitOrArchicad.mdx";
-import RevitWins from "./RevitWins.mdx";
-import ClosedBim from "./ClosedBim.mdx";
-import Cad from "./Cad.mdx";
-import WhatIsCad from "./WhatIsCad.mdx";
-
 export const content: Toc = [
+  {
+    element: (
+      <>
+        <Image
+          src="/images/week03/tools-of-the-imagination.jpg"
+          alt="Tools of Imagination"
+          href="https://books.google.com.na/books?id=1WUIIh99xDkC&printsec=frontcover&source=gbs_book_other_versions_r&cad=4#v=onepage&q&f=false"
+        />
+        <h4>
+          <em>
+            The architect needs the right tools for the job. [...] The newest
+            tool, the computer, is really an entirely new toolbox full of
+            software to solve old and new problems.
+          </em>
+          <h5>
+            <br />
+            <em>Susan Piedmont Palladino - Tools of the imagination .</em>
+          </h5>
+        </h4>
+      </>
+    ),
+  },
+  {
+    element: (
+      <h4>
+        <em>
+          It is through the mastery of the full range of tools that the images
+          in the architect’s mind are translated into drawings and models, and
+          from the drawings and the models into buildings. Whether a simple
+          pencil or computer software, these tools give form to the wishes and
+          the instructions of the architect, but they are also active shapers of
+          what is possible.
+        </em>
+        <h5>
+          <br />
+          <em>Susan Piedmont Palladino - Tools of the imagination .</em>
+        </h5>
+      </h4>
+    ),
+  },
   {
     element: (
       <Image
@@ -74,50 +103,6 @@ export const content: Toc = [
       </h4>
     ),
   },
-
-  {
-    element: (
-      <Image
-        src="/images/week03/tools-of-the-imagination.jpg"
-        alt="Tools of Imagination"
-        caption="Tools of Imagination - Susan Piedmont-Palladino"
-        href="https://books.google.com.na/books?id=1WUIIh99xDkC&printsec=frontcover&source=gbs_book_other_versions_r&cad=4#v=onepage&q&f=false"
-      />
-    ),
-  },
-  {
-    element: (
-      <h4>
-        <em>
-          The architect needs the right tools for the job. [...] The newest
-          tool, the computer, is really an entirely new toolbox full of software
-          to solve old and new problems.
-        </em>
-        <h5>
-          <br />
-          <em>Susan Piedmont Palladino - Tools of the imagination .</em>
-        </h5>
-      </h4>
-    ),
-  },
-  {
-    element: (
-      <h4>
-        <em>
-          It is through the mastery of the full range of tools that the images
-          in the architect’s mind are translated into drawings and models, and
-          from the drawings and the models into buildings. Whether a simple
-          pencil or computer software, these tools give form to the wishes and
-          the instructions of the architect, but they are also active shapers of
-          what is possible.
-        </em>
-        <h5>
-          <br />
-          <em>Susan Piedmont Palladino - Tools of the imagination .</em>
-        </h5>
-      </h4>
-    ),
-  },
   {
     element: (
       <Image
@@ -144,7 +129,41 @@ export const content: Toc = [
       This was the moment when, for most architects, programming ceased to be an open language to interact with the machine, and thus became a tool.\
       In some cases, an accessible tool, but in most cases, an utterly opaque one.",
   },
-  { element: <Cad /> },
+  {
+    element: (
+      <>
+        <h1>❔</h1>
+        <br />
+        <h2>Do you know what CAD stands for?</h2>
+      </>
+    ),
+  },
+  {
+    element: (
+      <>
+        <h1>❔</h1>
+        <br />
+        <h2>
+          Computer Aided <b>Design</b> or Computer Aided <b>Drafting</b>?
+        </h2>
+      </>
+    ),
+  },
+  {
+    element: (
+      <>
+        <h1>❔</h1>
+        <br />
+        <h2>
+          <b>CADD</b> : Computer Aided Design and Drafting
+        </h2>
+        <br />
+        <h2>
+          <b>CAAD</b> : Computer Aided Architectural Desing
+        </h2>
+      </>
+    ),
+  },
   {
     element: (
       <embed
@@ -155,7 +174,6 @@ export const content: Toc = [
       ></embed>
     ),
   },
-  { element: <WhatIsCad /> },
   {
     element: (
       <Image
@@ -165,27 +183,122 @@ export const content: Toc = [
       />
     ),
   },
-  { element: <WhatIsBim /> },
-  { element: <BimOrCad /> },
   {
-    element: <BimBooks />,
-    notes:
-      "Weygnant’s definition is a good baseline for tool-oriented people; Saxon’s explanation is for client/business-oriented people; Eastman is the father of BIM, and has the most technically correct definition > we’ll look at all three",
+    element: (
+      <>
+        <h1>❔</h1>
+        <br />
+        <h2>Do you know what BIM stands for?</h2>
+      </>
+    ),
   },
-
-  { element: <Levels /> },
-
-  { element: <RevitWins /> },
-  { element: <ClosedBim /> },
   {
-    element: <RevitOrArchicad />,
+    element: (
+      <>
+        <h1>Building Information Model or Building Information Modelling</h1>
+        <br />
+        <h5>... definitelly not a great acronym</h5>
+      </>
+    ),
+  },
+  {
+    element: (
+      <>
+        <h1>❔</h1>
+        <br />
+        <h2>How is BIM different from CAD?</h2>
+      </>
+    ),
+  },
+  {
+    element: (
+      <Image
+        src="/images/week03/bim-books.png"
+        alt="BIM Books"
+        caption={"Three fundamental BIM books: Weygnant’s, Saxon’s, Eastman’s"}
+      />
+    ),
     notes:
-      "BIM has been defined as a communication and collaboration tool or process originally created to connect people, processes, and data.\
+      "Weygnant’s definition is a good baseline for tool-oriented people; Saxon’s explanation is for client/business-oriented people; Eastman is considered the father of BIM, and has the most technically correct definition",
+  },
+  {
+    element: (
+      <Image
+        src="/images/week03/levels.png"
+        alt="BIM Books"
+        caption={"BIM Maturity Levels"}
+      />
+    ),
+    notes: "",
+  },
+  {
+    element: (
+      <Image
+        src="/images/week03/revitorarchicad.png"
+        alt="Revit or Archicad?"
+        caption={
+          "BIM authoring tools have been fighting to become the starndard tool in the industry. Who is winning?"
+        }
+      />
+    ),
+    notes: "",
+  },
+  {
+    element: (
+      <Video
+        src="https://www.youtube.com/embed/iuiichHnV9g?si=qgNvNyF7h8Pwk5S1"
+        title="Revit wins"
+        caption={"Revit has become the most popular BIM software in the world"}
+      />
+    ),
+    notes: "",
+  },
+  {
+    element: (
+      <Image
+        src="/images/week03/closed-bim.png"
+        alt="Closed BIM"
+        caption={
+          "Proprietary BIM authoring tools and formats turn BIM into a closed process\
+          BIM has been defined as a communication and collaboration tool or process originally created to connect people, processes, and data.\
 However, that is not exactly the way the Architecture Engineering and Construction or AEC industry is using BIM.\
-The status quo is to work with proprietary solutions and closed file formats.",
+The status quo is to work with proprietary solutions and closed file formats."
+        }
+      />
+    ),
+    notes: "",
   },
   {
-    element: <Mandates />,
+    element: (
+      <>
+        <h1>❔</h1>
+        <br />
+        <h2>Why should we care?</h2>
+      </>
+    ),
+  },
+  {
+    element: (
+      <>
+        <h1>💸</h1>
+        <br />
+        <h2>Proprietary software&apos;s licences are expensive!</h2>
+        <br />
+        <h3>
+          Revit standar licence for architecture products cost at least $3.000
+          USD 💵💵💵
+        </h3>
+      </>
+    ),
+  },
+  {
+    element: (
+      <Image
+        src="/images/week03/mandates.png"
+        alt="BIM Mandates"
+        caption={"By BICP Global BIM Study - Lessons For Ireland's Programme"}
+      />
+    ),
     notes:
       "Government BIM mandates.\
     Countries all over the word are becoming to regulate the use of BIM. So if one wants to participate in public works and even non public projects BIM is a requirement.\
