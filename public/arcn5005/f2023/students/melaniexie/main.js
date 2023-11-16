7f89af42533bd7f620c0edcd3bb60c87cb874b2c
melaniexie <xieqiongyi@gmail.com>
2023-11-16 03:01:32 -0500

    js

--- public/arcn5005/f2023/students/melaniexie/main.js
+++ public/arcn5005/f2023/students/melaniexie/main.js
@@ -1,105 +1,120 @@
-const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xbbbbbb );
-const size = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
-const aspect = size.width / size.height;
+let scene, camera, renderer, directionalLight, controls, loader;
+let threeCanvas;
+let cube,cube2,Torus,mesh,groundGeometry,mesh2;
 
-//Set up Camera
-const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+function init(){
+    scene = new THREE.Scene();
 
-
-//Sets up the renderer, fetching the canvas of the HTML
-const threeCanvas = document.getElementById("three-canvas");
-const renderer = new THREE.WebGLRenderer({
-  canvas: threeCanvas,
-  alpha: true,
-});
-
-renderer.setSize(size.width, size.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-document.body.appendChild(renderer.domElement);
-
-//Creates grids and axes in the scene
-const grid = new THREE.GridHelper(10, 10);
-scene.add(grid);
-
-const axes = new THREE.AxesHelper();
-axes.material.depthTest = false;
-axes.renderOrder = 1;
-scene.add(axes);
-
-
-const geometry = new THREE.BoxGeometry(1.5, 1.5, 1.5);
-
-const yellowMaterial = new THREE.MeshNormalMaterial();
-const blueMaterial = new THREE.MeshLambertMaterial({ color: 0x0000ff });
-const redMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 });
-const greenMaterial = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
-
-const yellowCube = new THREE.Mesh(geometry, yellowMaterial);
-const blueCube = new THREE.Mesh(geometry, blueMaterial);
-const redCube = new THREE.Mesh(geometry, redMaterial);
-const greenCube = new THREE.Mesh(geometry, greenMaterial);
-
-
-yellowCube.position.z = -3;
-blueCube.position.x = -3;
-redCube.position.x = 3;
-greenCube.position.z = 3;
-
-scene.add(yellowCube);
-scene.add(blueCube);
-// scene.add(redCube);
-scene.add(greenCube);
-
-const gltfLoader = new THREE.GLTFLoader();
-
-let mesh;
-gltfLoader.load(
-    "/models/Figure2.glb",
-    function (gltf) {
-      mesh = gltf.scene;
-      mesh.scale.x = 1;
-      mesh.scale.y = 1;
-      mesh.scale.z = 1;
-  
-      scene.add(mesh);
-    },
-    undefined,
-    function (error) {
-      console.error(error);
-    }
-  );
-
-  let mesh1;
-  gltfLoader.load(
-      "/models/Donut.glb",
-      function (gltf) {
-        mesh1 = gltf.scene;
-        mesh1.scale.x = 1;
-        mesh1.scale.y = 1;
-        mesh1.scale.z = 1;
-        
+    camera = new THREE.PerspectiveCamera(
+        75,window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set (-40,50,100);
+    scene.position.set (0,-30,0)
     
-        scene.add(mesh1);
-      },
-      undefined,
-      function (error) {
-        console.error(error);
-      }
-    );
+
+    renderer = new THREE.WebGLRenderer({
+        antialias:true,
+        canvas: threeCanvas,
+        alpha:true});
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    threeCanvas = document.getElementById("three-canvas");
+    
+    document.body.appendChild(renderer.domElement);
+
+    //Cube and Mesh
+    const geometry = new THREE.BoxGeometry(15,15,15);
+    const material = new THREE.MeshNormalMaterial();
+    cube = new THREE.Mesh( geometry, material);
+    cube.position.set(-50,20,0);
+    scene.add( cube);
+
+    const geometry1 = new THREE.IcosahedronGeometry(12,0);
+    const material1 = new THREE.MeshNormalMaterial();
+    cube2 = new THREE.Mesh(geometry1, material1);
+    cube2.position.set (-50, 60,0);
+    scene.add(cube2);
+
+    const geometry2 = new THREE.TorusKnotGeometry( 10, 3, 90, 16 );
+    const material2 = new THREE.MeshNormalMaterial();
+    Torus = new THREE.Mesh(geometry2,material2);
+    Torus.position.set (50, 40,40);
+    scene.add(Torus);
+    
+
+
+    const gltfLoader = new THREE.GLTFLoader();
+    const mesh ='/models/Figure2.glb';
+    gltfLoader.load(mesh, (gltf) => {
+        const root = gltf.scene;
+        root.scale.x = 10;
+        root.scale.y = 10;
+        root.scale.z = 10;
+        scene.add(root);
+
+    })
+    
+    const mesh2 ='/models/Donut1.glb';
+    gltfLoader.load(mesh2, (gltf) => {
+        const root2 = gltf.scene;
+        root2.scale.x = 25;
+        root2.scale.y = 25;
+        root2.scale.z = 25;
+        scene.add(root2);
+       })
 
 
 
-////text ///
-const fontLoader = new THREE.FontLoader();
 
-function createText(text, elevation = 0, textColor = "0x000000", size = 0.5) {
-  const textValue = text;
-  const textSize = size;
-  fontLoader.load("./fonts/helvetiker_regular.typeface.json", function (font) {
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+
+    //lighting
+    const lightColor = 0xffffff;
+    const ambientLight = new THREE.AmbientLight(lightColor, 1.0);
+    scene.add(ambientLight);
+
+    directionalLight = new THREE.DirectionalLight(lightColor, 0.8);
+    directionalLight.position.set(5, 30, 5);
+    directionalLight.target.position.set(0, 20, 0);
+    scene.add(directionalLight);
+    scene.add(directionalLight.target);
+
+    //background
+    loader = new THREE.TextureLoader();
+    const texture = loader.load(
+    '/images/library.jpg',
+    () => {
+      texture.mapping = THREE.EquirectangularReflectionMapping;
+      texture.colorSpace = THREE.SRGBColorSpace;
+      scene.background = texture;
+    });
+    
+    //ground
+    groundGeometry = new THREE.PlaneGeometry( 150,60 );
+    const groundMaterial = new THREE.MeshStandardMaterial( { roughness: 0.8, metalness: 0.4 } );
+    const ground = new THREE.Mesh( groundGeometry, groundMaterial );
+    ground.rotation.x = Math.PI * - 0.5;
+    scene.add( ground );
+
+    const textureLoader = new THREE.TextureLoader();
+    textureLoader.load( 'textures/concrete.png', function ( map ) {
+
+        map.wrapS = THREE.RepeatWrapping;
+        map.wrapT = THREE.RepeatWrapping;
+        map.anisotropy = 32;
+        map.repeat.set( 8,8);
+        map.colorSpace = THREE.SRGBColorSpace;
+        groundMaterial.map = map;
+        groundMaterial.needsUpdate = true;
+
+    } );
+
+    ////text ///
+    const fontLoader = new THREE.FontLoader();
+
+    function createText(text, elevation = 20, textColor = "0000ff", size = 5) {
+    const textValue = text;
+    const textSize = size;
+    fontLoader.load("./fonts/helvetiker_regular.typeface.json", function (font) {
     const textGeo = new THREE.TextGeometry(textValue, {
       font: font,
       size: textSize,
@@ -117,71 +132,73 @@ function createText(text, elevation = 0, textColor = "0x000000", size = 0.5) {
     const textMaterial = new THREE.MeshLambertMaterial({ color: color });
     const text = new THREE.Mesh(textGeo, textMaterial);
 
-    text.position.x = 2;
+    text.position.x = 20;
     text.position.y = elevation;
 
     scene.add(text);
   });
 }
 
-createText("Nicolas Arellano", 5, "0XFF00FF");
-createText("- Architect from PUC", 3, "0XFF0000");
-createText("- Research team lead at CIMS", 2, "0XFF0000");
-createText("- PhD candidate at ASAU", 1, "0XFF0000");
-createText("- Amateur programmer", 0, "0XFF0000");
+createText("THREE.JS EXERCISE", 50, "0XFF00FF");
+createText("-Blender Model", 40, "0XFF0000");
+createText("-Photogrammetry Model", 30, "0XFF0000");
+createText("-Three.js geometry", 20, "0XFF0000");
+//createText("- Amateur programmer", 0, "0XFF0000");
 
-//camera and scene position
-camera.position.z = 13;
-camera.position.x = 5;
-camera.position.y = 2;
 
-scene.position.x = -5;
-scene.position.z = 5;
-scene.position.y = -3;
 
-const controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-
-//Creates the lights of the scene
-const lightColor = 0xffffff;
-
-const ambientLight = new THREE.AmbientLight(lightColor, 1.0);
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(lightColor, 0.8);
-directionalLight.position.set(5, 10, 5);
-directionalLight.target.position.set(0, 3, 0);
-scene.add(directionalLight);
-scene.add(directionalLight.target);
-
-// Animate
-function animate() {
-  requestAnimationFrame(animate);
-
-  if (mesh) mesh.rotation.y += 0.0;
-
-  yellowCube.rotation.x += 0.01;
-  yellowCube.rotation.y += 0.01;
-
-  blueCube.rotation.x += 0.02;
-  blueCube.rotation.y -= 0.01;
-
-  redCube.rotation.x -= 0.01;
-  redCube.rotation.y -= 0.02;
-
-  greenCube.rotation.x += 0.02;
-  greenCube.rotation.y -= 0.01;
-
-  renderer.render(scene, camera);
+    
 }
 
+
+
+function animate(){
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y -= 0.02;
+
+    cube2.rotation.x += 0.01;
+    cube2.rotation.y -= 0.01;
+    renderer.render(scene,camera);
+
+    Torus.rotation.x +=0.02;
+    Torus.rotation.y -=0.02;
+
+    if (mesh) mesh.rotation.y += 0.1;
+    
+}
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth,window.innerHeight);
+}
+window.addEventListener('resize', onWindowResize, false);
+
+init();
 animate();
 
-//Adjust the viewport to the size of the browser
-window.addEventListener("resize", () => {
-  size.width = window.innerWidth;
-  size.height = window.innerHeight;
-  camera.aspect = size.width / size.height;
-  camera.updateProjectionMatrix();
-  renderer.setSize(size.width, size.height);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- public/arcn5005/f2023/students/melaniexie/test.js
+++ public/arcn5005/f2023/students/melaniexie/test.js
@@ -1,14 +1,20 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 30, window.innerWidth/window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
+const threeCanvas = document.getElementById("three-canvas");
+
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+document.body.appendChild(renderer.domElement);
+
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 // add cube
-var geometry = new THREE.BoxGeometry( 20, 20, 20);
-var material = new THREE.MeshNormalMaterial();
-var cube = new THREE.Mesh( geometry, material );
+const geometry = new THREE.BoxGeometry( 20, 20, 20);
+const material = new THREE.MeshNormalMaterial();
+const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 // update cube vertices
@@ -23,12 +29,12 @@ cube.rotation.y = -0.25;
 
 camera.position.z = 100;
 
-var light = new THREE.PointLight( 0xFFFF00 );
+const light = new THREE.PointLight( 0xFFFF00 );
 light.position.set( 10, 0, 25 );
 scene.add( light );
 
 
-var render = function () {
+const render = function () {
   requestAnimationFrame( render );
   cube.rotation.x += 0.05;
   renderer.render(scene, camera);
