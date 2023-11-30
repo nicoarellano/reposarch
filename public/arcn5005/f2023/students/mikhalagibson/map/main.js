047507d5b783d7804998667c23d71de2a8978222
mikhalagibson <65245013+mikhalag@users.noreply.github.com>
2023-11-30 01:13:34 -0500

    map

---
+++ public/arcn5005/f2023/students/mikhalagibson/map/main.js
@@ -0,0 +1,349 @@
+const places = {
+    'type': 'FeatureCollection',
+    'features': [
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Cultural',
+                'description': '<h2>Circular fortresses from the Viking Age</h2> <p>Significance: cultural</p> <p>Country: Denmark</p>'
+                
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [9.25468, 56.99523]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Cultural',
+                'description': '<h2>Ancient Jericho/Tell es-Sultan</h2> <p>Significance: cultural</p> <p>Country: State of Palestine</p>'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [35.26386, 31.52167]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Cultural',
+                'description': '<h2>Hopewell Ceremonial Earthworks</h2> <p>Significance: cultural</p> <p>Country: United States of America</p>'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-82.264582, 40.31317]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Cultural',
+                'description': '<h2>ESMA Museum and Place of Memory - Former clandestine center of detention, torture and extermination</h2> <p>Significance: cultural</p> <p>Country: Argentina</p>'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-58.27552, -34.321191]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Cultural',
+                'description': 
+                '<h2>Gordon</h2> <p>Significance: cultural</p> <p>Country: Turkey</p>'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [31.5910, 39.3836]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Cultural',
+                'description': "<h2>Tak'alik Ab'aj National Archaeological Park</h2> <p>Significance: cultural</p> <p>Country: Guatemala</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-91.4357, 14.3819]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Cultural',
+                'description': "<h2>Medieval Jewish heritage of Erfurt</h2> <p>Significance: cultural</p> <p>Country: Germany</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [11.14524, 50.584319]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Cultural',
+                'description': "<h2>Cultural landscape of ancient tea forests of Jingmai Mountain in Pu'er</h2> <p>Significance: cultural</p> <p>Country: China</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [100.027, 22.113]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Cultural',
+                'description': "<h2>Silk Roads: Zeravshan-Karakoum corridor</h2> <p>Significance: cultural</p> <p>Country: Uzbekistan</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [69.41801, 39.262936]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Cultural',
+                'description': "<h2>Tr’ondëk–Klondike</h2> <p>Significance: cultural</p> <p>Country: Canada</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-139.262381, 64.3125]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Natural',
+                'description': "<h2>Odzala-Kokoua Forest Massif</h2> <p>Significance: natural properties</p> <p>Country: Congo</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [14.5137, 1.208]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Natural',
+                'description': "<h2>Volcanoes and forests of Montagne Pelée and the peaks of northern Martinique</h2> <p>Significance: natural</p> <p>Country: France (Martinique)</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-61.10331, 14.492389]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Natural',
+                'description': "<h2>Anticosti</h2> <p>Significance: natural properties</p> <p>Country: Canada</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-63.1443, 49.3219]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Natural',
+                'description': "<h2>Turanian deserts with cold winter</h2> <p>Significance: natural properties</p> <p>Country: Kazakhstan, Turkménistan</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [79.23, 44.133]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Natural',
+                'description': "<h2>Nyungwe National Park</h2> <p>Significance: natural properties</p> <p>Country: Rwanda</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [29.14504, -2.331908]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Natural',
+                'description': "<h2>'Uruq Bani Ma'arid</h2> <p>Significance: natural properties</p> <p>Country: Saudi Arabia</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [45.3554, 19.2150]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Natural',
+                'description': "<h2>Bale Mountains National Park</h2> <p>Significance: natural properties</p> <p>Country: Ethiopia</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [39.4456, 6.4752]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Boundary Changes (Cultural)',
+                'description': "<h2>Koutammakou, the land of the Batammariba</h2><p> Significance: cultural</p> <p>Country: Benin</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-1.608, 10.8531]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Boundary Changes (Cultural)',
+                'description': "<h2>Historic center of Guimarães and Couros area</h2><p> Significance: cultural</p> <p>Country: Portugal</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-8.1734, 41.26348]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Boundary Changes (Natural)',
+                'description': "<h2>Ha Long Bay – Cat Ba archipelago</h2><p> Significance: natural</p> <p>Country: Vietnam</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [107.940, 20.4958]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Boundary Changes (Natural)',
+                'description': "<h2>Ha Long Bay – Cat Ba archipelago</h2><p> Significance: natural</p> <p>Country: Vietnam</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [107.940, 20.4958]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Boundary Changes (Natural)',
+                'description': "<h2>Hyrcanian forests</h2><p> Significance: natural</p> <p>Country: Azerbaijan</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [55.43274, 37.25173]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Boundary Changes (Natural)',
+                'description': "<h2>The dry forests of Andrefana</h2><p> Significance: natural</p> <p>Country: Madagascar</p>"
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [44.46259, -18.394168]
+            }
+        },
+    ]
+};
+
+const filterGroup = document.getElementById("filter-group");
+const map = new maplibregl.Map({
+    container: "map",
+    style:
+        'https://api.maptiler.com/maps/satellite/style.json?key=42hWlNIdOqEy0E4oMcxo#1/0/0',
+    center: [-5.07532, 22.02233],
+    zoom: 2
+});
+
+
+map.on('load', () => {
+    // Add a GeoJSON source containing place coordinates and information.
+    map.addSource('places', {
+        'type': 'geojson',
+        'data': places
+    });
+
+    places.features.forEach((feature) => {
+        const symbol = feature.properties['icon'];
+        const layerID = `poi-${symbol}`;
+
+        // Add a layer for this symbol type if it hasn't been added already.
+        if (!map.getLayer(layerID)) {
+            map.addLayer({
+                'id': layerID,
+                'type': 'circle',
+                'source': 'places',
+                'paint': {
+                    'circle-radius': 8,
+                    'circle-color': '#FFFFFF'
+                },
+                'filter': ['==', 'icon', symbol]
+            });
+           let popup;
+           
+            map.on('mouseenter', layerID, (e) => {
+                map.getCanvas().style.cursor = 'pointer';
+
+                const coordinates = e.features[0].geometry.coordinates.slice();
+                const description = e.features[0].properties.description;
+
+                if (!popup) {
+                    popup = new maplibregl.Popup({
+                        closeButton: false,
+                        closeOnClick: false
+                    }).setLngLat(coordinates)
+                      .setHTML(description)
+                      .addTo(map);
+                } else {
+                    popup.setLngLat(coordinates)
+                         .setHTML(description);
+                }
+            });
+
+            map.on('mouseleave', layerID, () => {
+                map.getCanvas().style.cursor = '';
+                if (popup) {
+                    popup.remove(); // Remove the popup if it exists
+                    popup = undefined; // Reset the popup variable
+                }
+            });
+
+            
+            // Add checkbox and label elements for the layer.
+            const input = document.createElement('input');
+            input.type = 'checkbox';
+            input.id = layerID;
+            input.checked = true;
+            filterGroup.appendChild(input);
+
+            const label = document.createElement('label');
+            label.setAttribute('for', layerID);
+            label.textContent = symbol;
+            filterGroup.appendChild(label);
+            //
+            
+            // When the checkbox changes, update the visibility of the layer.
+            input.addEventListener('change', (e) => {
+                map.setLayoutProperty(
+                    layerID,
+                    'visibility',
+                    e.target.checked ? 'visible' : 'none'
+                );
+            });
+        }
+    });
+
+});
