88cad70e40ebe86caa105b47adf5c6f6b6f3ab5f
mikhalagibson <65245013+mikhalag@users.noreply.github.com>
2023-11-30 10:52:40 -0500

    additional info

--- public/arcn5005/f2023/students/mikhalagibson/map/main.js
+++ public/arcn5005/f2023/students/mikhalagibson/map/main.js
@@ -5,8 +5,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Cultural',
-                'description': '<h2>Circular fortresses from the Viking Age</h2> <p>Significance: cultural</p> <p>Country: Denmark</p>'
-                
+                'description': '<h2>Circular fortresses from the Viking Age</h2> <p>Significance: cultural</p> <p>Country: Denmark</p><p>Criterion: (iii)(iv)</p>'
             },
             'geometry': {
                 'type': 'Point',
@@ -17,7 +16,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Cultural',
-                'description': '<h2>Ancient Jericho/Tell es-Sultan</h2> <p>Significance: cultural</p> <p>Country: State of Palestine</p>'
+                'description': '<h2>Ancient Jericho/Tell es-Sultan</h2> <p>Significance: cultural</p> <p>Country: State of Palestine</p><p>Criterion: (iii)(iv)</p>'
             },
             'geometry': {
                 'type': 'Point',
@@ -28,7 +27,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Cultural',
-                'description': '<h2>Hopewell Ceremonial Earthworks</h2> <p>Significance: cultural</p> <p>Country: United States of America</p>'
+                'description': '<h2>Hopewell Ceremonial Earthworks</h2> <p>Significance: cultural</p> <p>Country: United States of America</p><p>Criterion: (i)(iii)</p>'
             },
             'geometry': {
                 'type': 'Point',
@@ -39,7 +38,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Cultural',
-                'description': '<h2>ESMA Museum and Place of Memory - Former clandestine center of detention, torture and extermination</h2> <p>Significance: cultural</p> <p>Country: Argentina</p>'
+                'description': '<h2>ESMA Museum and Place of Memory - Former clandestine center of detention, torture and extermination</h2> <p>Significance: cultural</p> <p>Country: Argentina</p><p>Criterion: (vi)</p>'
             },
             'geometry': {
                 'type': 'Point',
@@ -51,7 +50,7 @@ const places = {
             'properties': {
                 'icon': 'Cultural',
                 'description': 
-                '<h2>Gordon</h2> <p>Significance: cultural</p> <p>Country: Turkey</p>'
+                '<h2>Gordon</h2> <p>Significance: cultural</p> <p>Country: Turkey</p><p>Criterion: (iii)</p>'
             },
             'geometry': {
                 'type': 'Point',
@@ -62,7 +61,43 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Cultural',
-                'description': "<h2>Tak'alik Ab'aj National Archaeological Park</h2> <p>Significance: cultural</p> <p>Country: Guatemala</p>"
+                'description': 
+                '<h2>Kaunas, modernist city: an architecure of optimism, 1919-1939</h2> <p>Significance: cultural</p> <p>Country: Lithuania</p><p>Criterion: (iv)</p>'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [23.5545, 54.5349]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Cultural',
+                'description': 
+                '<h2>Koh Ker: archaeological site of ancient Lingpura or Chok Gargyar</h2> <p>Significance: cultural</p> <p>Country: Cambodia</p><p>Criterion: (ii)(iv)</p>'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [104.3214, 13.4659]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Cultural',
+                'description': 
+                '<h2>The Persian caravanseri</h2> <p>Significance: cultural</p> <p>Country: Iran</p><p>Criterion: (ii)(iii)</p>'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [51.251249, 35.33007]
+            }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'icon': 'Cultural',
+                'description': "<h2>Tak'alik Ab'aj National Archaeological Park</h2> <p>Significance: cultural</p> <p>Country: Guatemala</p><p>Criterion: (ii)(iii)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -73,7 +108,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Cultural',
-                'description': "<h2>Medieval Jewish heritage of Erfurt</h2> <p>Significance: cultural</p> <p>Country: Germany</p>"
+                'description': "<h2>Medieval Jewish heritage of Erfurt</h2> <p>Significance: cultural</p> <p>Country: Germany</p><p>Criterion: (iv)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -84,7 +119,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Cultural',
-                'description': "<h2>Cultural landscape of ancient tea forests of Jingmai Mountain in Pu'er</h2> <p>Significance: cultural</p> <p>Country: China</p>"
+                'description': "<h2>Cultural landscape of ancient tea forests of Jingmai Mountain in Pu'er</h2> <p>Significance: cultural</p> <p>Country: China</p><p>Criterion: (iii)(v)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -95,7 +130,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Cultural',
-                'description': "<h2>Silk Roads: Zeravshan-Karakoum corridor</h2> <p>Significance: cultural</p> <p>Country: Uzbekistan</p>"
+                'description': "<h2>Silk Roads: Zeravshan-Karakoum corridor</h2> <p>Significance: cultural</p> <p>Country: Uzbekistan</p><p>Criterion: (ii)(iii)(v)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -106,7 +141,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Cultural',
-                'description': "<h2>Tr’ondëk–Klondike</h2> <p>Significance: cultural</p> <p>Country: Canada</p>"
+                'description': "<h2>Tr’ondëk–Klondike</h2> <p>Significance: cultural</p> <p>Country: Canada</p><p>Criterion: (iv)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -117,7 +152,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Natural',
-                'description': "<h2>Odzala-Kokoua Forest Massif</h2> <p>Significance: natural properties</p> <p>Country: Congo</p>"
+                'description': "<h2>Odzala-Kokoua Forest Massif</h2> <p>Significance: natural properties</p> <p>Country: Congo</p><p>Criterion: (ix)(x)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -128,7 +163,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Natural',
-                'description': "<h2>Volcanoes and forests of Montagne Pelée and the peaks of northern Martinique</h2> <p>Significance: natural</p> <p>Country: France (Martinique)</p>"
+                'description': "<h2>Volcanoes and forests of Montagne Pelée and the peaks of northern Martinique</h2> <p>Significance: natural</p> <p>Country: France (Martinique)</p><p>Criterion: (viii)(x)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -139,7 +174,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Natural',
-                'description': "<h2>Anticosti</h2> <p>Significance: natural properties</p> <p>Country: Canada</p>"
+                'description': "<h2>Anticosti</h2> <p>Significance: natural properties</p> <p>Country: Canada</p><p>Criterion: (viii)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -150,7 +185,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Natural',
-                'description': "<h2>Turanian deserts with cold winter</h2> <p>Significance: natural properties</p> <p>Country: Kazakhstan, Turkménistan</p>"
+                'description': "<h2>Turanian deserts with cold winter</h2> <p>Significance: natural properties</p> <p>Country: Kazakhstan, Turkménistan</p><p>Criterion: (ix)(x)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -161,7 +196,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Natural',
-                'description': "<h2>Nyungwe National Park</h2> <p>Significance: natural properties</p> <p>Country: Rwanda</p>"
+                'description': "<h2>Nyungwe National Park</h2> <p>Significance: natural properties</p> <p>Country: Rwanda</p><p>Criterion: (x)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -172,7 +207,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Natural',
-                'description': "<h2>'Uruq Bani Ma'arid</h2> <p>Significance: natural properties</p> <p>Country: Saudi Arabia</p>"
+                'description': "<h2>'Uruq Bani Ma'arid</h2> <p>Significance: natural properties</p> <p>Country: Saudi Arabia</p><p>Criterion: (vii)(ix)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -183,7 +218,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Natural',
-                'description': "<h2>Bale Mountains National Park</h2> <p>Significance: natural properties</p> <p>Country: Ethiopia</p>"
+                'description': "<h2>Bale Mountains National Park</h2> <p>Significance: natural properties</p> <p>Country: Ethiopia</p><p>Criterion: (vii)(x)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -194,7 +229,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Boundary Changes (Cultural)',
-                'description': "<h2>Koutammakou, the land of the Batammariba</h2><p> Significance: cultural</p> <p>Country: Benin</p>"
+                'description': "<h2>Koutammakou, the land of the Batammariba</h2><p> Significance: cultural</p> <p>Country: Benin</p><p>Criterion: (v)(vi)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -205,7 +240,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Boundary Changes (Cultural)',
-                'description': "<h2>Historic center of Guimarães and Couros area</h2><p> Significance: cultural</p> <p>Country: Portugal</p>"
+                'description': "<h2>Historic center of Guimarães and Couros area</h2><p> Significance: cultural</p> <p>Country: Portugal</p><p>Criterion: (ii)(iii)(iv)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -216,7 +251,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Boundary Changes (Natural)',
-                'description': "<h2>Ha Long Bay – Cat Ba archipelago</h2><p> Significance: natural</p> <p>Country: Vietnam</p>"
+                'description': "<h2>Ha Long Bay – Cat Ba archipelago</h2><p> Significance: natural</p> <p>Country: Vietnam</p><p>Criterion: (vii)(viii)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -227,18 +262,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Boundary Changes (Natural)',
-                'description': "<h2>Ha Long Bay – Cat Ba archipelago</h2><p> Significance: natural</p> <p>Country: Vietnam</p>"
-            },
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [107.940, 20.4958]
-            }
-        },
-        {
-            'type': 'Feature',
-            'properties': {
-                'icon': 'Boundary Changes (Natural)',
-                'description': "<h2>Hyrcanian forests</h2><p> Significance: natural</p> <p>Country: Azerbaijan</p>"
+                'description': "<h2>Hyrcanian forests</h2><p> Significance: natural</p> <p>Country: Azerbaijan</p><p>Criterion: (ix)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -249,7 +273,7 @@ const places = {
             'type': 'Feature',
             'properties': {
                 'icon': 'Boundary Changes (Natural)',
-                'description': "<h2>The dry forests of Andrefana</h2><p> Significance: natural</p> <p>Country: Madagascar</p>"
+                'description': "<h2>The dry forests of Andrefana</h2><p> Significance: natural</p> <p>Country: Madagascar</p><p>Criterion: (vii)(ix)(x)</p>"
             },
             'geometry': {
                 'type': 'Point',
@@ -258,6 +282,7 @@ const places = {
         },
     ]
 };
+
 
 const filterGroup = document.getElementById("filter-group");
 const map = new maplibregl.Map({
