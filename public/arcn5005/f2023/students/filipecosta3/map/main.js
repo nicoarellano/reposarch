eecf48ce4ac304b43332872b276793a86325948e
filipeo-costa <144391473+filipeo-costa@users.noreply.github.com>
2023-12-05 13:39:19 -0500

    Cleanup of folder and Map Assignment Submission

--- public/arcn5005/f2023/students/filipecosta3/map/main.js
+++ public/arcn5005/f2023/students/filipecosta3/map/main.js
@@ -6,6 +6,25 @@ const map = new maplibregl.Map({
   zoom: 16
 });
 
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      // Move the camera forward
+      camera.position.z -= 0.05;
+      break;
+    case "ArrowDown":
+      // Move the camera backward
+      camera.position.z += 0.05;
+    case "ArrowLeft":
+      // Move the camera left
+      camera.position.x -= 0.05;
+      break;
+    case "ArrowRight":
+      // Move the camera right
+      camera.position.x += 0.05;
+  }
+});
+
 map.on('load', () => {
   //Pokespot Marker
   map.loadImage(
@@ -452,6 +471,271 @@ map.on('load', () => {
                   "Location":"Robertson Hall, Ottawa, ON K1S, Canada",
                   }
                 },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.693677,45.383882 ]
+                  },
+                  "properties": {
+                  "Name":"Nesbitt Biology Building ",
+                  "Type":"Pokespot",
+                  "Location":"Nesbitt Biology Bldg, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.693524,45.383624 ]
+                  },
+                  "properties": {
+                  "Name":"Butterfly House",
+                  "Type":"Pokespot",
+                  "Location":"Nesbitt Biology Bldg, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.693190,45.383438 ]
+                  },
+                  "properties": {
+                  "Name":"Cassia Family Golden Honey Locust",
+                  "Type":"Pokespot",
+                  "Location":"Nesbitt Biology Bldg, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.692911,45.383547 ]
+                  },
+                  "properties": {
+                  "Name":"New Sun Tribute",
+                  "Type":"Pokespot",
+                  "Location":"Nesbitt Biology Bldg, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.692207,45.383928 ]
+                  },
+                  "properties": {
+                  "Name":"Kitigànensag Shed",
+                  "Type":"Pokespot",
+                  "Location":"National Wildlife Research Centre, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.692776,45.384280 ]
+                  },
+                  "properties": {
+                  "Name":"National Wildlife Research Centre",
+                  "Type":"Pokespot",
+                  "Location":"National Wildlife Research Centre, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.693642,45.384550 ]
+                  },
+                  "properties": {
+                  "Name":"Carleton Technology And Training Centre",
+                  "Type":"Pokespot",
+                  "Location":"1125 Colonel By Dr, Ottawa, ON K1S 5B6, Canada"
+                  ,
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.692030,45.384126 ]
+                  },
+                  "properties": {
+                  "Name":"Kitigànensag Community Garden",
+                  "Type":"Pokespot",
+                  "Location":"National Wildlife Research Centre, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.694778,45.384899 ]
+                  },
+                  "properties": {
+                  "Name":"Carleton Maintenance Building",
+                  "Type":"Pokespot",
+                  "Location":"University Dr, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.694633,45.385274 ]
+                  },
+                  "properties": {
+                  "Name":"Carleton University Campus Map - Athletics",
+                  "Type":"Pokespot",
+                  "Location":"1125 Colonel By Dr, Ottawa, ON K1S 5B6, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.693677,45.385469 ]
+                  },
+                  "properties": {
+                  "Name":"Carleton Ice House",
+                  "Type":"Pokespot",
+                  "Location":"9376 University Dr, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.697701,45.386420 ]
+                  },
+                  "properties": {
+                  "Name":"Lennox And Addington Residence",
+                  "Type":"Pokespot",
+                  "Location":"Lennox and Addington, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.697222,45.387356 ]
+                  },
+                  "properties": {
+                  "Name":"Stormont And Dundas House",
+                  "Type":"Pokespot",
+                  "Location":"Stormont House, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.697943,45.387580 ]
+                  },
+                  "properties": {
+                  "Name":"St. Patrick Building",
+                  "Type":"Pokespot",
+                  "Location":"Campus Ave, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.698080,45.387890 ]
+                  },
+                  "properties": {
+                  "Name":"Folded Metal",
+                  "Type":"Pokespot",
+                  "Location":"Campus Ave, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.697006,45.388399 ]
+                  },
+                  "properties": {
+                  "Name":"Carleton University Campus Map - Residences North Entrance",
+                  "Type":"Pokespot",
+                  "Location":"Dundas House, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.696442,45.389142 ]
+                  },
+                  "properties": {
+                  "Name":"Campus Map",
+                  "Type":"Pokespot",
+                  "Location":"98Q3+MC Nepean, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.693783,45.386336 ]
+                  },
+                  "properties": {
+                  "Name":"Ravens Wall of Champions",
+                  "Type":"Pokespot",
+                  "Location":"Physical Recreation Centre, 65 University Rd, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.693702,45.385998 ]
+                  },
+                  "properties": {
+                  "Name":"Ravens Skywalk",
+                  "Type":"Pokespot",
+                  "Location":"Physical Recreation Centre, 65 University Rd, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.692830,45.386591 ]
+                  },
+                  "properties": {
+                  "Name":"Alumni Hall",
+                  "Type":"Pokespot",
+                  "Location":"3328 Bronson Ave, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.694161,45.386773 ]
+                  },
+                  "properties": {
+                  "Name":"The Fieldhouse",
+                  "Type":"Pokespot",
+                  "Location":"Fieldhouse, 85 University Rd, Ottawa, ON K1S, Canada",
+                  }
+                },
+                {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.692571,45.386194 ]
+                  },
+                  "properties": {
+                  "Name":"Carleton Ice Rink Arena",
+                  "Type":"Pokespot",
+                  "Location":"Ice House, Ottawa, ON K1S, Canada",
+                  }
+                },
                ]
                }
           });
@@ -464,7 +748,7 @@ map.on('load', () => {
               'layout': {
                   'icon-image': 'custom-marker',
                   'icon-size': 0.5
-
+                
               }
           });
       }
@@ -496,9 +780,23 @@ map.loadImage(
                    "properties": {
                    "Name":"Carleton University Courtyard",
                    "Type":"Gym",
+                   "PrimaryTeam":"Team Mystic",
                    "Location":"34 Library Rd, Ottawa, ON K1S 5R1, Canada",
                    }
                  },
+                 {
+                  "type": "Feature",
+                  "geometry": {
+                     "type": "Point",
+                     "coordinates":  [ -75.691849,45.386997 ]
+                  },
+                  "properties": {
+                  "Name":"Carleton University North Entrance Sign ",
+                  "Type":"Gym",
+                  "PrimaryTeam":"Team Instinct",
+                  "Location":"3328 Bronson Ave, Ottawa, ON K1S 5R1, Canada",
+                  }
+                },
                  {
                   "type": "Feature",
                   "geometry": {
@@ -508,6 +806,7 @@ map.loadImage(
                   "properties": {
                   "Name":"Dunton Tower",
                   "Type":"Gym",
+                  "PrimaryTeam":"Team Valor",
                   "Location":"Dunton Tower, Ottawa, ON K1S, Canada",
                   }
                 },
@@ -520,6 +819,7 @@ map.loadImage(
                   "properties": {
                   "Name":"Carleton Quad",
                   "Type":"Gym",
+                  "PrimaryTeam":"Team Mystic",
                   "Location":"Paterson Hall, Ottawa, ON K1S, Canada",
                   }
                 },
@@ -532,6 +832,7 @@ map.loadImage(
                   "properties": {
                   "Name":"Orenda OT-F-3 Jet Engine",
                   "Type":"Gym",
+                  "PrimaryTeam":"Team Mystic",
                   "Location":"Mackenzie Building, Ottawa, ON K1S, Canada",
                   }
                 },
@@ -544,6 +845,7 @@ map.loadImage(
                   "properties": {
                   "Name":"World Flags",
                   "Type":"Gym",
+                  "PrimaryTeam":"Team Valor",
                   "Location":"University Centre, 1125 Colonel By Dr, Ottawa, ON K1S 5B6, Canada",
                   }
                 },
@@ -556,6 +858,7 @@ map.loadImage(
                   "properties": {
                   "Name":"Gandhi Statue",
                   "Type":"Gym",
+                  "PrimaryTeam":"Team Mystic",
                   "Location":"9376 University Dr, Ottawa, ON K1S, Canada",
                   }
                 },
@@ -568,6 +871,7 @@ map.loadImage(
                   "properties": {
                   "Name":"Professor David Morice Leigh Farr Memorial Bench",
                   "Type":"Gym",
+                  "PrimaryTeam":"Team Mystic",
                   "Location":"River Building, 9376 University Dr, Ottawa, ON K1S, Canada",
                   }
                 },
@@ -591,6 +895,135 @@ map.loadImage(
 )
 });
 
+ //Carleton University
+ map.on('load', () => {
+  map.addSource ('CarletonUniversity', {
+    "type": 'geojson',
+    "data": {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "properties": {
+            "Neighbourhood": "CarletonUniversity",
+            "stroke": "#555555",
+            "stroke-width": 2,
+            "stroke-opacity": 1,
+            "fill": "#088F8F",
+            "fill-opacity": 0.2
+          },
+          "geometry": {
+            "coordinates": [
+              [
+                [
+                  -75.6963279501042,
+                  45.39374193625795
+                ],
+                [
+                  -75.69955324394601,
+                  45.39161055521913
+                ],
+                [
+                  -75.70039829439769,
+                  45.390229450624076
+                ],
+                [
+                  -75.70000991831189,
+                  45.3880371715386
+                ],
+                [
+                  -75.69903470434721,
+                  45.385079662633274
+                ],
+                [
+                  -75.6994165417231,
+                  45.383564545544374
+                ],
+                [
+                  -75.70107333105776,
+                  45.38144900579377
+                ],
+                [
+                  -75.70146576239074,
+                  45.380364333431004
+                ],
+                [
+                  -75.70133447473223,
+                  45.378881519391
+                ],
+                [
+                  -75.7008715609701,
+                  45.37892711657406
+                ],
+                [
+                  -75.6974303714912,
+                  45.381416367390045
+                ],
+                [
+                  -75.69202343990241,
+                  45.383261088805256
+                ],
+                [
+                  -75.69197090040002,
+                  45.384415484236655
+                ],
+                [
+                  -75.69028864279564,
+                  45.384941655638926
+                ],
+                [
+                  -75.68996160462189,
+                  45.38512333105683
+                ],
+                [
+                  -75.6963279501042,
+                  45.39374193625795
+                ]
+              ]
+            ],
+            "type": "Polygon"
+          },
+          "id": 12
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "name": "Carleton University",
+                        },
+          "geometry": {
+              "type": "Point",
+              "coordinates": [-75.696467, 45.383831]
+                      }
+        }
+      ]
+    }
+    });
+
+  map.addLayer({
+    'id': 'CarletonUniversity',
+    'type': 'fill',
+    'source': 'CarletonUniversity',
+    'layout': {},
+    'paint': {
+            'fill-color': '#088F8F',
+            'fill-opacity': 0.2,
+              }
+    });
+
+    map.addLayer({
+      'id': 'Carletonuniversity-label',
+      'type': 'symbol',
+      'source': 'CarletonUniversity',
+      'layout':  {
+        'text-field': ['get', 'name'],
+        'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
+        'text-radial-offset': 0.2,
+        'text-justify': 'auto',
+          },
+        'filter': ['==', '$type', 'Point']
+    });
+  });
+
 //Pokespot Popups
 map.on('click', 'Pokespot', (e) => {
   const coordinates = e.features[0].geometry.coordinates.slice();
@@ -600,7 +1033,7 @@ map.on('click', 'Pokespot', (e) => {
   // Ensure that if the map is zoomed out such that multiple
   // copies of the feature are visible, the popup appears
   // over the copy being pointed to.
-  while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) 
+  while (Math.abs(e.lngLat.lng - coordinates[0]) > 200) 
     {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
@@ -608,7 +1041,7 @@ map.on('click', 'Pokespot', (e) => {
       new maplibregl.Popup()
           .setLngLat(coordinates)
           .setHTML(`
-              <h3>${Pokespot.properties.Name}</h3>
+              <h3>${Pokespot.properties.Name} <img src="./Pokemon Sprites/Pokeball.png" alt="OHome Image" style="width:100%; max-width:30px; height:auto;"></h3>
               <p>Type: ${Pokespot.properties.Type}</p>
               <p>Location: ${Pokespot.properties.Location}</p>
               `)
@@ -632,8 +1065,9 @@ map.on('click', 'Gym', (e) => {
       new maplibregl.Popup()
           .setLngLat(coordinates)
           .setHTML(`
-              <h3>${Gym.properties.Name}</h3>
+              <h3>${Gym.properties.Name} <img src="./Pokemon Sprites/Greatball.png" alt="OHome Image" style="width:100%; max-width:30px; height:auto;"></h3>
               <p>Type: ${Gym.properties.Type}</p>
+              <p>PrimaryTeam: ${Gym.properties.PrimaryTeam}</p>
               <p>Location: ${Gym.properties.Location}</p>
               `)
           .addTo(map);
