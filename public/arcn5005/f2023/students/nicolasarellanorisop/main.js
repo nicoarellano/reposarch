ea197163c9b7cfce3e5cf53771629792db623325
Nico <nico14@gmail.com>
2023-12-06 13:56:39 -0500

    keegan and nico about

--- public/arcn5005/f2023/students/nicolasarellanorisop/main.js
+++ public/arcn5005/f2023/students/nicolasarellanorisop/main.js
@@ -56,7 +56,7 @@ const gltfLoader = new THREE.GLTFLoader();
 let mesh;
 
 gltfLoader.load(
-  "./models/justin.glb",
+  "./three/models/justin.glb",
   function (gltf) {
     mesh = gltf.scene;
     mesh.scale.x = 3;
@@ -76,29 +76,32 @@ const fontLoader = new THREE.FontLoader();
 function createText(text, elevation = 0, textColor = "0x000000", size = 0.5) {
   const textValue = text;
   const textSize = size;
-  fontLoader.load("./fonts/helvetiker_regular.typeface.json", function (font) {
-    const textGeo = new THREE.TextGeometry(textValue, {
-      font: font,
-      size: textSize,
-      height: 0.1,
-      curveSegments: 4,
-      bevelEnabled: true,
-      bevelThickness: 0.1,
-      bevelSize: 0.0,
-      bevelOffset: 0,
-      bevelSegments: 5,
-    });
+  fontLoader.load(
+    "./three/fonts/helvetiker_regular.typeface.json",
+    function (font) {
+      const textGeo = new THREE.TextGeometry(textValue, {
+        font: font,
+        size: textSize,
+        height: 0.1,
+        curveSegments: 4,
+        bevelEnabled: true,
+        bevelThickness: 0.1,
+        bevelSize: 0.0,
+        bevelOffset: 0,
+        bevelSegments: 5,
+      });
 
-    const color = new THREE.Color();
-    color.setHex(textColor);
-    const textMaterial = new THREE.MeshLambertMaterial({ color: color });
-    const text = new THREE.Mesh(textGeo, textMaterial);
+      const color = new THREE.Color();
+      color.setHex(textColor);
+      const textMaterial = new THREE.MeshLambertMaterial({ color: color });
+      const text = new THREE.Mesh(textGeo, textMaterial);
 
-    text.position.x = 2;
-    text.position.y = elevation;
+      text.position.x = 2;
+      text.position.y = elevation;
 
-    scene.add(text);
-  });
+      scene.add(text);
+    }
+  );
 }
 
 createText("Nicolas Arellano", 5, "0XFF00FF");
