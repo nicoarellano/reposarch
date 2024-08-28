276909a0d29a5f619f9738c39086d25ebe05a583
Nico Arellano <95719187+nicoarellano@users.noreply.github.com>
2024-08-28 11:50:55 -0400

    new course outline (#415)

--- app/arcn5005/(main)/course-outline/page.tsx
+++ app/arcn5005/(main)/course-outline/page.tsx
@@ -1,7 +1,7 @@
-import PdfViewer from "@/components/Common/PdfViewer";
+import PdfViewer from '@/components/Common/PdfViewer';
 
 export default function CourseDescription() {
   return (
-    <PdfViewer src="/arcn5005/terms/f2023/ASAU ARCN5005 f2023- Course Outline.pdf" />
+    <PdfViewer src="/arcn5005/terms/f2024/ASAU ARCN5005 f2024 - Course Outline.pdf" />
   );
 }
