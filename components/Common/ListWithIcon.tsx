68b88c740eec3d16d6c44a684830e29a64ff024f
Nico <nico14@gmail.com>
2024-09-04 06:52:35 -0400

    change to presentation page

--- components/Common/ListWithIcon.tsx
+++ components/Common/ListWithIcon.tsx
@@ -1,11 +1,11 @@
-"use client";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import { IconButton, ListSubheader } from "@mui/material";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+'use client';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { IconButton, ListSubheader } from '@mui/material';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface Props {
   list: { title: string }[];
@@ -16,29 +16,28 @@ interface Props {
 export default function ListWithIcon({
   list = [],
   icon,
-  subheader = "Topics",
+  subheader = 'Topics',
 }: Props) {
   const path = usePathname();
   return (
     <List
       sx={{
-        bgcolor: "background.paper",
-        overflowY: "auto",
-        overflowX: "hidden",
+        bgcolor: 'background.paper',
+        overflowY: 'auto',
+        overflowX: 'hidden',
       }}
       component="nav"
       aria-labelledby="nested-list-subheader"
       subheader={<ListSubheader component="div">{subheader}</ListSubheader>}
     >
       {list.map((item: { title: string; url?: string }, index) => {
-        console.log(item);
         return (
           <ListItem key={index}>
             <ListItemIcon>
               {Boolean(item.url) ? (
                 <Link
                   href={
-                    item.url?.startsWith("http") || item.url?.startsWith("/")
+                    item.url?.startsWith('http') || item.url?.startsWith('/')
                       ? item.url
                       : `${path}/${item.url}`
                   }
