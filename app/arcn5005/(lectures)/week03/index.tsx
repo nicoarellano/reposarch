ca6abf649f4c3c68cd81d30981d173af7884bd66
Nico Arellano <95719187+nicoarellano@users.noreply.github.com>
2024-09-18 20:46:38 -0400

    adding recording 3 and william (#432)
    * new course outline
    
    * change to presentation page
    
    * non reversing announcements
    
    * adding week 1 class recording
    
    * adding recording and fixing typo on outline
    
    * course outline
    
    * new announcements embeding class recording
    
    * fixing course outline
    
    * fixing office hours
    
    * week 2 updates
    
    * week 2 addition
    
    * mdx week 2
    
    * uploading class recordings and adding jh
    
    * week 2 (#425) (#426)
    
    * new course outline
    
    * change to presentation page
    
    * non reversing announcements
    
    * adding week 1 class recording
    
    * adding recording and fixing typo on outline
    
    * course outline
    
    * new announcements embeding class recording
    
    * fixing course outline
    
    * fixing office hours
    
    * week 2 updates
    
    * week 2 addition
    
    * mdx week 2
    
    * adding jh
    
    * embeding videos week 2
    
    * fixing times of course outline
    
    * embedign videos (#428) (#429)
    
    * new course outline
    
    * change to presentation page
    
    * non reversing announcements
    
    * adding week 1 class recording
    
    * adding recording and fixing typo on outline
    
    * course outline
    
    * new announcements embeding class recording
    
    * fixing course outline
    
    * fixing office hours
    
    * week 2 updates
    
    * week 2 addition
    
    * mdx week 2
    
    * uploading class recordings and adding jh
    
    * week 2 (#425) (#426)
    
    * new course outline
    
    * change to presentation page
    
    * non reversing announcements
    
    * adding week 1 class recording
    
    * adding recording and fixing typo on outline
    
    * course outline
    
    * new announcements embeding class recording
    
    * fixing course outline
    
    * fixing office hours
    
    * week 2 updates
    
    * week 2 addition
    
    * mdx week 2
    
    * adding jh
    
    * embeding videos week 2
    
    * fixing times of course outline
    
    * Update index.tsx bonsai bim (#430)
    
    * adding william and w3 recordings
    
    * Update index.tsx bonsai bim (#431)
    
    * fixing recording

--- app/arcn5005/(lectures)/week03/index.tsx
+++ app/arcn5005/(lectures)/week03/index.tsx
@@ -26,9 +26,11 @@ export const content: Toc = [
   {
     element: (
       <Video
-        src="https://www.youtube.com/embed/GHW0PLXnqhs?si=0AtxbAz1BRhrZBCz"
-        title="Class 3  - Representation tools and processes for architects"
-        caption={'Class 3  - Representation tools and processes for architects'}
+        src="https://www.youtube.com/embed/wruFGy9uPkA?si=bhI3khTc5aK10Swt"
+        title="Class 3  - Representation tools and processes for architects (fall 2024)"
+        caption={
+          'Class 3  - Representation tools and processes for architects (fall 2024)'
+        }
       />
     ),
   },
