9a16e0cb0997ff7324173ab3cb660e3575ffc331
Nico <nico14@gmail.com>
2024-11-07 20:31:40 -0500

    logos

--- app/arcn5005/(lectures)/week06/index.tsx
+++ app/arcn5005/(lectures)/week06/index.tsx
@@ -136,40 +136,6 @@ export const content: Toc = [
   {
     element: (
       <Image
-        src="/images/osarch/ifc-logo.png"
-        alt="ifc definition"
-        href="https://www.buildingsmart.org/standards/bsi-standards/industry-foundation-classes/"
-        caption={'IFC - Industry Foundation Classes'}
-      />
-    ),
-    notes: '',
-  },
-  {
-    element: (
-      <Image
-        src="/images/osarch/ids.png"
-        alt="ids definition"
-        href="https://www.buildingsmart.org/standards/bsi-standards/information-delivery-specification-ids/"
-        caption={'IDS - Information Delivery Specification'}
-      />
-    ),
-    notes: '',
-  },
-  {
-    element: (
-      <Image
-        src="/images/osarch/bcf.png"
-        alt="bcf definition"
-        href="https://www.buildingsmart.org/standards/bsi-standards/bim-collaboration-format/"
-        caption={'BCF - BIM Collaboration Format'}
-      />
-    ),
-    notes: '',
-  },
-
-  {
-    element: (
-      <Image
         src="/images/osarch/openbim-definition.png"
         alt="openbim definition"
         href="https://wiki.osarch.org/index.php?title=OpenBIM"
@@ -228,7 +194,7 @@ it is very hard to achieve openBIM',
   {
     element: (
       <Image
-        src="/images/osarch/ifc.png"
+        src="/images/osarch/ifc-logo.png"
         alt="IFC"
         href="https://www.buildingsmart.org/standards/bsi-standards/industry-foundation-classes/"
         caption="Industry Foundation Classes (IFC)"
@@ -242,6 +208,29 @@ it is very hard to achieve openBIM',
     It is an open, international standard (ISO 16739-1:2018) and promotes vendor-neutral, or agnostic, and usable capabilities across a wide range of hardware devices, \
     software platforms, and interfaces for many different use cases. More about IFC, its uses and adoption may be found here.',
   },
+  {
+    element: (
+      <Image
+        src="/images/osarch/ids.png"
+        alt="ids definition"
+        href="https://www.buildingsmart.org/standards/bsi-standards/information-delivery-specification-ids/"
+        caption={'IDS - Information Delivery Specification'}
+      />
+    ),
+    notes: '',
+  },
+  {
+    element: (
+      <Image
+        src="/images/osarch/bcf.png"
+        alt="bcf definition"
+        href="https://www.buildingsmart.org/standards/bsi-standards/bim-collaboration-format/"
+        caption={'BCF - BIM Collaboration Format'}
+      />
+    ),
+    notes: '',
+  },
+
   {
     element: (
       <Video
