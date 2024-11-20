62c1b22fe3408f533b6ae32a301afdd0a7c377b1
Faressmar <maria.fares115@gmail.com>
2024-11-20 18:54:00 -0500

    A3

---
+++ public/arcn5005/terms/f2024/students/mariafares/map/ihop.js
@@ -0,0 +1,19 @@
+export const waffles = [
+    {
+      name: "Waffle House #1745",
+      city: "Scranton",
+      state: "Pennsylvania",
+      image: "",
+      latitude: 41.48776150432355, 
+      longitude: -75.69524130898354,
+    },
+    {
+        name: "Waffle House #378",
+        city: "Raleigh",
+        state: "North Carolina",
+        image: "",
+        latitude: 41.48776150432355, 
+        longitude: -75.69524130898354,
+      },
+
+]
