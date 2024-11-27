7fd4304fb7583dd7f85df2e2cbb76ca919a19b5b
reposarch-zk <zelalkaya@cmail.carleton.ca>
2024-11-27 08:44:02 -0500

    testing

--- public/arcn5005/terms/f2024/students/zelalkaya/map/main.js
+++ public/arcn5005/terms/f2024/students/zelalkaya/map/main.js
@@ -1,17 +1,13 @@
-// Initialize map centered on Montreal
 const map = L.map('map').setView([45.503477, -73.587048], 10);
 
-// Add OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Add marker at Montreal
 const marker = L.marker([45.5017, -73.5673]).addTo(map);
 marker.bindPopup("<b>Welcome to Montreal!</b>").openPopup();
 
-// Click event to show coordinates
 map.on('click', function(e) {
     const popup = L.popup()
         .setLatLng(e.latlng)
@@ -19,11 +15,10 @@ map.on('click', function(e) {
         .openOn(map);
 });
 
-// Info panel
 const info = L.control();
 info.onAdd = function(map) {
     this._div = L.DomUtil.create('div', 'info-panel');
-    this._div.innerHTML = '<h4>Montreal Map</h4>Click anywhere on the map to see coordinates';
+    this._div.innerHTML = '<h4>Click anywhere on the map to see coordinates</h4>';
     return this._div;
 };
 info.addTo(map);
