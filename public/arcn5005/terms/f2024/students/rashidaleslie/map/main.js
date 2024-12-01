e9e62f5c5576cde6058b707c190a699188b8de7b
Rashida-Leslie <rashidaleslie@cmail.carleton.ca>
2024-11-30 22:22:10 -0500

    new points

--- public/arcn5005/terms/f2024/students/rashidaleslie/map/main.js
+++ public/arcn5005/terms/f2024/students/rashidaleslie/map/main.js
@@ -1,6 +1,34 @@
 const map = new maplibregl.Map({
     container: 'map',
     style: 'style.json',
-    center: [-96.81595411588849,49.77154223266254],
-    zoom: 3
+    center: [-122.9697, 49.2488], // [Longitude, Latitude]
+    zoom: 12
+});
+
+map.on('load', function () {
+    // Add a GeoJSON source
+    map.addSource('places', {
+        type: 'geojson',
+        data: 'places.geojson' // Path to your GeoJSON file
+    });
+
+    // Add a custom marker image
+    map.loadImage('star-marker.png', function (error, image) {
+        map.addImage('custom-marker', image, { pixelRatio: 2 });
+       
+    });
+
+    // Add a layer using the custom marker
+    map.addLayer({
+        id: 'points',
+        type: 'symbol',
+        source: 'places',
+        layout: {
+            'icon-image': 'custom-marker', // Use your custom marker here
+            'icon-size': 1.5,
+            'text-field': ['get', 'name'], 
+            'text-offset': [0, 1.5],
+            'text-anchor': 'top'
+        }
+    });
 });
