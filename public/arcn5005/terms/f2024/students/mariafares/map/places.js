a34e0acbfa2ff5f17fa4548e2181b3f38303c83b
Faressmar <maria.fares115@gmail.com>
2024-12-02 13:01:36 -0500

    Map Attempt 3

--- public/arcn5005/terms/f2024/students/mariafares/map/places.js
+++ public/arcn5005/terms/f2024/students/mariafares/map/places.js
@@ -1,19 +1,38 @@
-export const waffles = [
-    {
-      name: "Spawning Point",
-      city: "Scranton",
-      state: "Pennsylvania",
-      image: "",
-      latitude: 41.48776150432355, 
-      longitude: -75.69524130898354,
-    },
-    {
-        name: "Waffle House #378",
-        city: "Raleigh",
-        state: "North Carolina",
-        image: "",
-        latitude: 41.48776150432355, 
-        longitude: -75.69524130898354,
-      },
-
-]
+export const places = [
+  {
+    name: "Joseph Brant Hospital",
+    city: "Burlington",
+    image: "/places/01 JB.png",
+    latitude: 43.31673728479727,
+    longitude: -79.80216895581788,
+  },
+  {
+    name: "Jackson's Square Farmers Market ",
+    city: "Hamilton",
+    image: "/places/02 jk.jpg",
+    latitude: 43.25907061509756,
+    longitude: 79.87010221220656,
+  },
+  {
+    name: "Tansley Woods",
+    city: "Burlington",
+    image: "/places/03 TW.jpg",
+    latitude: 43.38211648823582,
+    longitude: -79.80076891044253,
+  },
+  {
+    name: "Binbrook Fair Grounds",
+    city: "Hamilton",
+    image: "/places/04 bf.jpg",
+    latitude: 43.122766023257824,
+    longitude: -79.8039542665564,
+  },
+  {
+    name: "St Mary & St Athanasius Coptic Orthodox Church",
+    city: "Missisauga",
+    image: "/places/05 sm.jpg",
+    latitude: 43.580319323207206,
+    longitude: -79.68340184410881,
+  },
+ 
+];
