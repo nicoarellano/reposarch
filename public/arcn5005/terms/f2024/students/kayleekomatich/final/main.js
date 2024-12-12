5316e6ef5d417a8d461488e2b7ce5b60c9d15be9
kayleekomatich <kayleekomatich@cmail.carleton.ca>
2024-12-11 21:37:05 -0500

    Final (#877)
    * assignment2
    
    * assignment 3
    
    * final assignment

---
+++ public/arcn5005/terms/f2024/students/kayleekomatich/final/main.js
@@ -0,0 +1,22 @@
+/* https://pajasevi.github.io/CSSnowflakes/ */
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml16');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml16 .letter',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 30 * i
+  }).add({
+    targets: '.ml16',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  
