f51dc52aa54f9af0e2935ec497420211d6aa15aa
joshharriman99 <josh.harriman@gmail.com>
2024-12-12 20:59:26 -0500

    yes

--- public/arcn5005/terms/f2024/students/joshharriman/final/main.js
+++ public/arcn5005/terms/f2024/students/joshharriman/final/main.js
@@ -133,20 +133,3 @@ function flyTo(map, lng, lat, zoom = 15, pitch = 50) {
 }
 
 
-
-
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-
