4c52ebeec0b85650e62ff900fc4fb197392630ec
Nico <nico14@gmail.com>
2026-03-10 21:05:43 -0400

    fixing next.config

--- next.config.js
+++ next.config.js
@@ -10,10 +10,22 @@ const withMDX = require('@next/mdx')({
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: false,
-  transpilePackages: ['three', '@thatopen/components', '@thatopen/components-front', '@thatopen/fragments'],
+  transpilePackages: [
+    'three',
+    '@thatopen/components',
+    '@thatopen/components-front',
+    '@thatopen/fragments',
+  ],
+  webpack: (config) => {
+    // Allow webpack/Terser to correctly handle ESM .mjs files from packages
+    // like @thatopen/fragments that ship worker files with import/export syntax.
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+    return config;
+  },
 };
-
-const withTM = require('next-transpile-modules')(['three']);
-module.exports = withTM();
 
 module.exports = withMDX(nextConfig);
