b536e97bb8f1ee27a2a1f8b28cbeaf0035bd125e
Nico <nico14@gmail.com>
2026-03-20 07:11:04 -0400

    reviewed dbb slides notes

--- app/deblackboxing/DbbSlides.ts
+++ app/deblackboxing/DbbSlides.ts
@@ -10,16 +10,20 @@ export const dbbSlides: Slides = [
             - Professor Keena\n\
             - Professor Bashandy\n\
             - Federica, Azam and Steve\n\
-            Thank you all for being here today and for your support in this tremendous milestone in my academic process\n\
-            I am Nicolás Arellano, and it is an honour to present my dissertation titled: Deblackboxing the Digital Toolbox: \n\
+            Thank you so much for being here today.\n\
+            I am Nicolás Arellano, and it is an honour to present my dissertation titled:\n\
+            Deblackboxing the Digital Toolbox: \n\
             Open-Source Toolmaking for Architects Beyond Proprietary Software and Closed File Formats."
   },
   {
     id: "abstract",
     title: "Abstract",
     url: "/deblackboxing/abstract",
-    notes: "As you may recall, my dissertation examines 'deblackboxing' as an architect-led practice of toolmaking. Specifically, it explores expanding the architect's disciplinary scope from merely operating software to authoring it.\n\
-    To reclaim agency, this research proposes Deblackboxing: the critical act of making digital instruments inspectable and modifiable. My research argues that this is best achieved through open-source toolmaking"
+    notes: "As you may recall, my dissertation examines 'deblackboxing' as an architect-led practice of toolmaking.\n\
+    Specifically, it explores expanding the architect's disciplinary scope from operating software to authorship.\n\
+    To reclaim agency, this research proposes Deblackboxing:\n\
+    which is the critical act of making digital tools inspectable and modifiable.\n\
+    My research argues that the way to achieve this is through open-source toolmaking."
   },
   {
     id: "chapters",
@@ -27,12 +31,15 @@ export const dbbSlides: Slides = [
     url: "/deblackboxing/chapters",
     notes: "The dissertation is organized into three main chapters.\n\
     ______________________________________________________________________________________\n\
-          - Chapter 1 establishes the problem and the need for a reflection on the relationship between architects and their representation software.\n\
+          - Chapter 1: Deblackboxing the Digital Toolbox for Architects,\n\
+          establishes the problem. The need for a reflection on the relationship between architects and their representation tools.\n\
+          and alternatives to the existing tools we have and use.\n\
     ______________________________________________________________________________________\n\
-          It also studies alternatives to the existing tools we have and use.\n\
-          - Chapter 2 details the educational framework I have developed and taught at Carleton University.\n\
+          - Chapter 2: Teaching Open-Source Toolmaking for Architects,\n\
+          details the educational framework I have developed and taught at Carleton University.\n\
     ______________________________________________________________________________________\n\
-          - Chapter 3 documents the creation and deployment of an open-source BIM/GIS platform, called Canada's Digital Twin.\n\
+          - Chapter 3: Developing a BIM/GIS Open-Source Tool,\n\
+          documents the creation and deployment of an open-source BIM/GIS platform, called Canada's Digital Twin.\n\
     ______________________________________________________________________________________\n\
           - And the conclusion is an optimistic invitation to a more open relationship with digital tools, one in which architects are active shapers of these instruments."
   },
@@ -41,59 +48,67 @@ export const dbbSlides: Slides = [
     title: "Obstacles: black boxes and digital oblivion (Chapter 1)",
     url: "/deblackboxing/obstacles",
     notes: "Let me know describe to you the problem that I have identified in my research\n\
-    The architecture profession faces a crisis of agency.\n\
+    Which is that the architecture profession faces a crisis of agency.\n\
     This crises of agency is presented through two interrelated phenomena:\n\
     ______________________________________________________________________________________\n\
     1) Digital Oblivion, which I define as the passive acceptance of these tools in our offices and schools.\n\
-    We ofter focus instruction and training on mastering interfaces instead of teaching how to understand the underlying system behind them.\n\
+    We ofter prioritize instruction and training on mastering interfaces\n\
+    instead of teaching how to understand the underlying system behind them.\n\
     ______________________________________________________________________________________\n\
-    2) And the dependency on Black Boxes, which are opaque systems that can be understood in terms of their inputs and outputs, but whose processes are not accessible to users.\n\
+    2) The second obstacle is the rooted dependency on Black Boxes,\n\
+    defined as opaque systems that can be understood in terms of their inputs and outputs, but whose processes are not accessible to users.\n\
     For example, proprietary software.\n\
-    Our digital workflows are controlled by restrictive licenses and closed file formats, transferring design control from the architect to the software vendors."
+    Our digital workflows are controlled by restrictive licenses and closed file formats,\n\
+    transferring design control from the architect to the software vendors."
   },
   {
     id: "epistemic-object",
     title: "Epistemic Objects: How to overcome the black boxes and digital oblivion?",
     url: "/deblackboxing/epistemic-object",
     notes: "For this dissertation, I didn't want to stay in the theory only.\n\
-In order to address these two obstacles, my research is grounded in two epistemic objects.\n\
-In this case, an epistemic object it’s the blend between theory and practice. Exploring the research question through diverse media. Or as Federica defines it in her book, an entwined process of thinking, writing, and making.\n\
-The epistemic objects are:\n\
+So in order to address these two obstacles, my research is grounded in two epistemic objects.\n\
+In this case, an epistemic object is the blend between theory and practice.\n\
+Exploring the research question through diverse media.\n\
+Or as Federica defines it in her book, an entwined process of thinking, writing, and making.\n\
+In this dissertation, the epistemic objects are:\n\
 \n\
 1) A university course to teach open-source digital toolmaking\n\
-2) The development of an open-soure digital tool\n\
+2) The development of an open-source digital tool\n\
 ______________________________________________________________________________________\n\
-1) I will now move into the first epistemic object. The course.\n\
+1) I will now move into the first one. The course.\n\
 ______________________________________________________________________________________\n\
-2) The developed of Canada’s Digital Twin."
+2) The development of Canada’s Digital Twin."
   },
   {
     id: "conclusion",
     title: "Conclusion",
     url: "/deblackboxing/conclusion",
-    notes: "To conclude, this dissertation argues that toolmaking is an essential skill for today's architect. It is the key to securing  agency.\n\
-Often Architects are passive users of 'black-boxed' software.\n\
+    notes: "To conclude, this dissertation argues that toolmaking is an essential skill for architects.\n\
+    It is key to securing agency.\n\
+When architects are passive users of 'black-boxed' software.\n\
 This hidden logic limits their ability to design and communicate design.\n\
-This research demonstrates a new path:\n\
+This research demonstrates a different path:\n\
 ______________________________________________________________________________________\n\
-1) Software is a space for creative intervention, not a boundary.\n\
+1) Software as a space for creative intervention, not a boundary.\n\
 ______________________________________________________________________________________\n\
-2) Programming can be taught as architectural literacy, turning students into toolmakers.\n\
+2) Programming can be taught as architectural literacy, turning architects into toolmakers.\n\
 ______________________________________________________________________________________\n\
-3) Canada’s Digital Twin proves that open-source toolmaking solves problems proprietary systems cannot.\n\
+3) Open-source toolmaking can solve problems proprietary systems cannot,\n\
+(Canada’s Digital Twin is an example of that)\n\
 ______________________________________________________________________________________\n\
 Architects can and should look inside user interface to understand and actively rewrite the biases of their tools.\n\
-The call to action is clear:\n\
+My call of action is that:\n\
 ______________________________________________________________________________________\n\
-1) Education should train authors, not just operators.\n\
+1) Education should focus on training authors, not just operators.\n\
 ______________________________________________________________________________________\n\
-2) Open-source collaboration keeps tools aligned with professional values, not commercial profit.\n\
+2) Open-source collaboration keeps tools aligned with professional values, not only commercial interests.\n\
 ______________________________________________________________________________________\n\
 The question is not if architects will use software, but if they will use it to define their own direction.\n\
-The profession can move from dependency to true sovereignty.\n\
+For the momentum I am witnessing in the last few years,\n\
+I truly believe the profession can move from dependency to true sovereignty.\n\
 \n\
 Thank you so much. I look forward to your questions.\n\
 ______________________________________________________________________________________\n\
-***If demos of the USE CASES are needed, go to https://reposarch.vercel.app/arcn5005/defence-dev/10"
+(If demos of the USE CASES are needed, go to https://reposarch.vercel.app/arcn5005/defence-dev/10)"
   },
 ];
